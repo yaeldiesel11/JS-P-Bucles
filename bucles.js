fe4dc9711197b7b2bd750ea7400329b8daa7d5ea
yaeldiesel11 <astr2109@gmail.com>
2024-04-20 02:14:29 -0400

    Init

---
+++ bucles.js
@@ -0,0 +1,11 @@
+// Sentirse cómodo con los bucles: for y while
+// Sentirse cómodo con los condicionales: if/else
+
+// Trabaja en los siguientes desafíos y carga tu trabajo cuando hayas terminado.
+
+// Imprimir impares 1-20 Usando un bucle, escribe un código que haga que console.logimprima todos los valores impares del 1 al 20.
+// Disminuir múltiplos de 3 Usando un bucle, escribe un código que haga que console.logimprima todos los valores que sean divisibles de manera uniforme por 3 del 100 al 0.
+// Imprime la secuencia Usando un bucle, escribe un código que haga que console.log imprima los valores en esta secuencia  4, 2.5, 1, -0.5, -2, -3.5.
+// Sigma Escribe código que sume todos los valores del 1 al 100 en una variable sum y, al final, console.log dé como resultado 1 + 2 + 3 + ... + 98 + 99 + 100. Deberíamos obtener 5050 al final.
+// Factorial Escribe código que multiplique todos los valores del 1 al 12 en una variable producty, al final, console.logdé como resultado  1 * 2 * 3 * ... * 10 * 11 * 12. Deberíamos obtener  479001600al final.
+
